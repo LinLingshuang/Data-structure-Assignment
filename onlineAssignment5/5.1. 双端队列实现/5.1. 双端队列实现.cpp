4bfb28264441936d39b85a7e5063100bb28276df
LinLingshuang <2025302114225@whu.edu.cn>
2026-03-31 19:51:26 +0800

    新作业

---
+++ onlineAssignment5/5.1. 双端队列实现/5.1. 双端队列实现.cpp
@@ -0,0 +1,195 @@
+﻿/*1. 双端队列实现
+双端队列实现
+一、问题描述
+实现一个双端队列（Deque），支持在队列两端进行插入和删除操作。
+支持的操作：
+PUSH_FRONT x：在队头插入元素x
+PUSH_BACK x：在队尾插入元素x
+POP_FRONT：移除队头元素，队空输出"EMPTY"
+POP_BACK：移除队尾元素，队空输出"EMPTY"
+FRONT：查看队头元素，队空输出"EMPTY"
+BACK：查看队尾元素，队空输出"EMPTY"
+二、输入形式
+第一行输入一个正整数m（1 ≤ m ≤ 100000），表示操作数量。
+接下来m行，每行一个操作，格式如上所述。
+三、输出形式
+对于需要输出的操作，输出对应的结果，每行一个。
+四、样例输入
+8
+PUSH_BACK 1
+PUSH_BACK 2
+PUSH_FRONT 3
+FRONT
+BACK
+POP_FRONT
+POP_BACK
+SIZE
+五、样例输出
+3
+2
+3
+2
+六、样例说明
+操作过程：
+PUSH_BACK 1：队列[1]
+PUSH_BACK 2：队列[1,2]
+PUSH_FRONT 3：队列[3,1,2]
+FRONT：队头为3
+BACK：队尾为2
+POP_FRONT：弹出3，输出3
+POP_BACK：弹出2，输出2
+SIZE：大小为1
+七、评分标准
+共6个测试用例。
+八、类库使用要求
+可以使用deque或手动实现双向链表，可以使用STL deque。*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+struct node {
+	int data;
+	node* next;
+	node() {
+		data = NULL;
+		next = NULL;
+	}
+	node(int val) {
+		data = val;
+		next = NULL;
+	}
+};
+
+class listDeque {
+private:
+	node* head;
+	node* end;
+public:
+	listDeque() {
+		end = new node;
+		end->next = end;
+		head = end;
+	}
+	listDeque(int num) {
+
+	}
+	void PUSH_BACK(int val) {
+		end->next = new node(val);
+		end = end->next;
+		end->next = head;
+	}
+	void PUSH_FRONT(int val) {
+		if (head == end) {
+			end->next = new node(val);
+			end = end->next;
+			end->next = head;
+		}
+		else{
+			node* p = head->next;
+			head->next = new node(val);
+			head->next->next = p;
+		}
+	}
+	bool POP_FRONT() {
+		if (head == end) {
+			cout << "EMPTY" << endl;
+			return false;
+		}
+		else {
+			cout << head->next->data << endl;
+			node* p = head->next;
+			head->next = head->next->next;
+			if (end == p) {
+				end = head;
+			}
+			delete p;
+			return true;
+		}
+	}
+	bool POP_BACK() {
+		if (head == end) {
+			cout << "EMPTY" << endl;
+			return false;
+		}
+		else {
+			cout << end->data << endl;
+			node* p = end;
+			node* q = head;
+			while (q->next != end) {
+				q = q->next;
+			}
+			end = q;
+			end->next = head;
+			delete p;
+			return true;
+		}
+	}
+	bool FRONT() {
+		if (head == end) {
+			cout << "EMPTY" << endl;
+			return false;
+		}
+		else {
+			cout << head->next->data << endl;
+			return true;
+		}
+	}
+	bool BACK() {
+		if (head == end) {
+			cout << "EMPTY" << endl;
+			return false;
+		}
+		else {
+			cout << end->data << endl;
+			return true;
+		}
+	}
+	int SIZE() {
+		int size = 0;
+		node* p = head;
+		while (p->next != head) {
+			p = p->next;
+			size++;
+		}
+		return size;
+	}
+};
+
+int main() {
+	int op;
+	cin >> op;
+	listDeque myDeque;
+	for (int i = 0; i < op; i++) {
+		string temp;
+		cin >> temp;
+		if (temp == "PUSH_BACK"){
+			int num;
+			cin >> num;
+			myDeque.PUSH_BACK(num);
+		}
+		else if (temp == "PUSH_FRONT") {
+			int num;
+			cin >> num;
+			myDeque.PUSH_FRONT(num);
+		}
+		else if (temp == "POP_FRONT") {
+			myDeque.POP_FRONT();
+		}
+		else if (temp == "POP_BACK") {
+			myDeque.POP_BACK();
+		}
+		else if (temp == "FRONT") {
+			myDeque.FRONT();
+		}
+		else if (temp == "BACK") {
+			myDeque.BACK();
+		}
+		else if (temp == "SIZE") {
+			cout<<myDeque.SIZE();
+		}
+	}
+
+
+
+}
+
---
+++ onlineAssignment5/5.2. 条件反转循环队列/5.2. 条件反转循环队列.cpp
@@ -0,0 +1,69 @@
+﻿/*2. 条件反转循环队列
+【问题描述】
+设计一个算法，利用一个顺序栈将一个循环队列中的所有元素倒过来，使得队头变为队尾，队尾变为队头。
+同时队列中存在特殊标记元素（值为 -1），这些元素在反转后必须保持原有的位置，其他元素需完全倒置。
+【输入形式】
+第一行输入一个整数n，表示循环队列的长度；
+第二行输入n个整数，以空格分隔，表示循环队列的元素值，输入按队列的顺序给出，即第一个数是队头元素，最后一个数是队尾元素。
+【输出形式】
+输出一行整数，表示倒置后的队列元素，元素之间以空格分隔，新的队头在最前面，新的队尾在最后面。
+【样例输入】
+5
+1 2 3 -1 5
+【样例输出】
+5 3 2 -1 1
+【样例说明】
+【评分标准】
+【类库使用要求】
+不可以使用STL中的stack、queue、vector等库函数，可以使用字符串STL处理输入。
+【说明】
+要求自己设计实现栈或队列类
+*/
+#include<iostream>
+using namespace std;
+
+struct circleQeque {
+	int head;
+	int rear;
+	int* qeque;
+	circleQeque(int n) {
+		head = 0;
+		qeque = new int[n];
+		rear = n - 1;
+		for (int i = 0; i < n; i++) {
+			int temp;
+			cin >> temp;
+			qeque[i] = temp;
+		}
+	}
+	void reverse() {
+		while (head < rear) {
+			if (qeque[head] != -1 && qeque[rear] != -1) {
+				int temp = qeque[head];
+				qeque[head] = qeque[rear];
+				qeque[rear] = temp;
+				head++;
+				rear--;
+			}
+			else if(qeque[head] == -1){
+				head++;
+			}
+			else if (qeque[rear] == -1) {
+				rear--;
+			}
+		}
+	}
+	void display(int n) {
+		for (int i = 0; i < n; i++) {
+			cout<<qeque[i]<<" ";
+		}
+	}
+};
+
+int main() {
+	int n;
+	cin >> n;
+	circleQeque myCircleQeque(n);
+	myCircleQeque.reverse();
+	myCircleQeque.display(n);
+}
