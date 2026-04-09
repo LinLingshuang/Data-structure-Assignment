64bc917e9f884893d422ba9bdf60d4443cfa57f6
LinLingshuang <2025302114225@whu.edu.cn>
2026-04-09 23:06:03 +0800

    更新百题巩基

---
+++ 百题筑基・数据结构每日一练（第一阶段）/14. 判断链表是否有环/14. 判断链表是否有环.cpp
@@ -0,0 +1,161 @@
+﻿//14. 判断链表是否有环
+//判断链表是否有环
+//一、问题描述
+//给定一个单链表，判断链表中是否存在环。如果存在环，输出"YES"；否则输出"NO"。
+//链表节点值为整数，输入格式特殊：每行输入两个整数val和next，val表示节点值，next表示下一个节点的位置索引（-1表示NULL）。
+//二、输入形式
+//第一行输入一个正整数n（1 ≤ n ≤ 100000），表示节点数量。
+//接下来n行，每行两个整数val和next，表示第i个节点的值和下一个节点的索引（1-indexed）。
+//三、输出形式
+//输出"YES"或"NO"，表示链表是否有环。
+//四、样例输入
+//4
+//1 2
+//2 3
+//3 4
+//4 2
+//五、样例输出
+//YES
+//六、样例说明
+//链表结构：1 → 2 → 3 → 4 → 2（形成环：2→3→4→2）
+//七、评分标准
+//共6个测试用例。
+//测试用例1-3：小规模数据，哈希法可过
+//测试用例4-6：大规模数据，要求使用快慢指针法（Floyd判圈算法）
+//八、类库使用要求
+//可以使用iostream、vector。要求使用Floyd判圈算法，空间复杂度O(1)。
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct Node {
+	int data;
+	int next;
+};
+int main() {
+	int num;
+	cin >> num;
+	vector<Node>my(num + 1);
+	for (int i = 1; i <= num; i++) {
+		int a, b;
+		cin >> a >> b;
+		my[i].data = i;
+		my[i].next = b;
+	}
+	for (int k = 1; k <= num; k++)
+	{
+		int slow = my[k].next;
+		int fast = my[k].next;
+		int dalay = 0;
+		while (fast != -1) {
+			fast = my[fast].next;
+			if (fast == my[fast].next) {
+				break;
+			}
+			if (fast == -1) {
+				break;
+			}
+			fast = my[fast].next;
+			slow = my[slow].next;
+			if (fast == slow && fast != -1 && fast != my[fast].next) {
+				cout << "YES";
+				return 0;
+			}
+		}
+
+	}
+	cout << "NO";
+	return 0;
+
+}
+/*
+int main() {
+	int num;
+	cin >> num;
+	vector<Node>my(10001000);
+	vector<int>no(num);
+	for (int i = 0; i < num; i++) {
+		int a, b;
+		cin >> a >> b;
+		no[i] = a;
+		my[a].data = a;
+		my[a].next = b;
+	}
+	for (int k = 0; k < num; k++)
+	{
+		int slow = my[no[k]].next;
+		int fast = my[no[k]].next;
+		int dalay = 0;
+		while (fast != -1) {
+			fast = my[fast].next;
+			if (fast == my[fast].next) {
+				break;
+			}
+			if (fast == -1) {
+				break;
+			}
+			fast = my[fast].next;
+			slow = my[slow].next;
+			if (fast == slow  && fast != -1 && fast != my[fast].next) {
+				cout << "YES";
+				return 0;
+			}
+		}
+
+	}
+	cout << "NO";
+	return 0;
+
+}
+*/
+/*
+int main() {
+	int num;
+	cin >> num;
+	vector<Node>myNode(num);
+	for (int i = 0; i < num; i++) {
+		cin >> myNode[i].data >> myNode[i].next;
+	}
+	for(int k=0;k<num;k++)
+	{
+		int slow = myNode[k].next;
+		int fast = myNode[k].next;
+		int dalay = 0;
+		bool change = false;
+		for (int i = 0; i < num; i++) {
+
+			for (int j = 0; j < num; j++) {
+				if (fast == myNode[j].data) {
+					fast = myNode[j].next;
+					dalay++;
+					change = true;
+					if (slow == fast && fast != -1 && change fast != myNode[k].next) {
+						cout << "YES";
+						return 0;
+					}
+					break;
+				}
+
+			}
+			for (int j = 0; j < num; j++) {
+				if (slow == myNode[j].data && dalay >= 10) {
+					slow = myNode[j].next;
+					dalay = 0;
+					if (slow == fast && fast != -1 && change fast != myNode[k].next) {
+						cout << "YES";
+						return 0;
+					}
+					break;
+				}
+			}
+			if (slow == fast&& fast!=-1 && change fast != myNode[k].next) {
+				cout << "YES";
+				return 0;
+			}
+		}
+
+	}
+	cout << "NO";
+	return 0;
+}
+*/
---
+++ 百题筑基・数据结构每日一练（第一阶段）/5. 顺序栈基本操作/5. 顺序栈基本操作.cpp
@@ -0,0 +1,144 @@
+﻿/*5. 顺序栈基本操作
+顺序栈基本操作
+一、问题描述
+实现一个顺序栈（固定容量），支持以下操作：
+
+PUSH x：将元素x压入栈顶，栈满时输出"FULL"
+
+POP：弹出栈顶元素，栈空时输出"EMPTY"
+
+TOP：查看栈顶元素，栈空时输出"EMPTY"
+
+SIZE：输出栈中元素个数
+
+二、输入形式
+第一行输入两个正整数n和m（1 ≤ n ≤ 100000, 1 ≤ m ≤ 100000），分别表示栈的最大容量和操作数量。
+接下来m行，每行一个操作，格式如上所述。
+
+三、输出形式
+对于POP、TOP操作，输出对应的值或错误信息。
+对于PUSH操作，栈满时输出"FULL"。
+每个输出占一行。
+
+四、样例输入
+
+3 8
+PUSH 1
+PUSH 2
+TOP
+POP
+SIZE
+PUSH 3
+PUSH 4
+PUSH 5
+五、样例输出
+
+2
+2
+1
+FULL
+六、样例说明
+操作过程：
+
+PUSH 1：栈[1]
+
+PUSH 2：栈[1,2]
+
+TOP：栈顶为2
+
+POP：弹出2，栈[1]
+
+SIZE：大小为1
+
+PUSH 3：栈[1,3]
+
+PUSH 4：栈[1,3,4]
+
+PUSH 5：栈满，输出FULL
+
+七、评分标准
+共6个测试用例。
+
+八、类库使用要求
+可以使用数组或vector实现，禁止使用STL stack。*/。
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+struct stack {
+	stack(int v) {
+		vepacity = v;
+		size = 0;
+		data = vector<int>(vepacity);
+	}
+
+	int vepacity;
+	int size;
+	vector<int> data;
+
+
+	void PUSH(int x) {
+		if (size == vepacity) {
+			cout << "FULL" << endl;
+			return;
+		}
+		else {
+			data[size] = x;
+			size++;
+		}
+	}
+	void POP() {
+		if (size == 0) {
+			cout << "EMPTY" << endl;
+			return;
+		}
+		else {
+			size--;
+			cout << data[size] << endl;
+		}
+	}
+	void TOP() {
+		if (size == 0) {
+			cout << "EMPTY" << endl;
+			return;
+		}
+		else {
+			cout << data[size - 1] << endl;
+		}
+	}
+	void SIZE() {
+		cout << size << endl;
+	}
+
+};
+
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	stack myStack(n);
+	for (int i = 0; i < m; i++) {
+		string op;
+		cin >> op;
+		if (op == "PUSH") {
+			int x;
+			cin >> x;
+			myStack.PUSH(x);
+		}
+		else if (op == "POP") {
+			myStack.POP();
+		}
+		else if (op == "TOP") {
+			myStack.TOP();
+		}
+		else if (op == "SIZE") {
+			myStack.SIZE();
+		}
+
+	}
+
+
+
+
+}
