f0e01636cfd1532f0eca2057a47ae2f407fe2e3a
LinLingshuang <2025302114225@whu.edu.cn>
2026-04-24 23:40:00 +0800

    第八周

---
+++ onlineAssignment8/8.1. 第六章 递归算法求解迷宫问题/8.1. 第六章 递归算法求解迷宫问题.cpp
@@ -0,0 +1,99 @@
+﻿/*1. 第六章 递归算法求解迷宫问题
+【问题描述】
+给定一个二维迷宫矩阵，其中：
+0 表示可以通过的路径。
+1 表示障碍物，无法通过。
+起点为左上角 (0, 0)，终点为右下角 (n-1, m-1)。
+请设计一个递归算法，判断是否存在一条从起点到终点的路径。如果存在，输出路径上节点数量；如果不存在，输出 -1。
+使用递归深度优先搜索（DFS）来探索迷宫：
+1.从起点开始，依次尝试四个方向（上、右、下、左）移动。
+2.每次移动时，检查当前位置是否越界或是否为障碍物。
+3.如果到达终点，则返回成功，并记录路径。
+4.如果所有方向都无法继续前进，则回溯。
+【输入形式】
+第一行为迷宫的n行m列值，接下来是n*m行的迷宫。
+【输出形式】
+如果找到迷宫路径，输出路径长度；
+如果没有找到路径，输出-1。
+【样例输入】
+5 5
+0 1 0 0 0
+0 1 0 1 0
+0 0 0 1 0
+0 1 1 1 0
+0 0 0 0 0
+【样例输出】
+13
+【样例说明】
+5行5列的矩阵，用递归算法按（上、右、下、左）顺时针方向找到路径上节点数量为9。
+【评分标准】
+【样例说明】
+可以使用STL库函数。*/
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct block {
+	int x;
+	int y;
+	bool canWalk;
+	bool haveWalked = false;
+};
+
+int solveMaze(vector<vector<block>>maze,int i,int j,int n,int m) {
+	if (i == n - 1 && j == m - 1) {
+		return 1;
+	}
+	else {	
+		maze[i][j].haveWalked = true;
+		if (i - 1 >= 0 && maze[i - 1][j].canWalk && !maze[i - 1][j].haveWalked) {
+			maze[i][j].haveWalked = true;
+			if (solveMaze(maze, i - 1, j, n, m) != -1) {
+				return solveMaze(maze, i - 1, j, n, m) + 1;
+			}
+		}
+		if (j + 1 < m && maze[i][j + 1].canWalk && !maze[i][j + 1].haveWalked) {
+			maze[i][j].haveWalked = true;
+			if (solveMaze(maze, i, j + 1, n, m) != -1) {
+				return solveMaze(maze, i, j + 1, n, m) + 1;
+			}
+		}
+		if (i + 1 < n && maze[i + 1][j].canWalk && !maze[i + 1][j].haveWalked) {
+			maze[i][j].haveWalked = true;
+			if (solveMaze(maze, i + 1, j, n, m) != -1) {
+				return solveMaze(maze, i + 1, j, n, m) + 1;
+			}
+		}
+		if (j - 1 >= 0 && maze[i][j - 1].canWalk && !maze[i][j - 1].haveWalked) {
+			maze[i][j].haveWalked = true;
+			if (solveMaze(maze, i, j - 1, n, m) != -1) {
+				return solveMaze(maze, i, j - 1, n, m) + 1;
+			}
+		}
+		else {
+			return -1;
+		}
+	}
+}
+
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	vector<vector<block>>maze(n, vector<block>(m));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			int blockState;
+			cin >> blockState;
+			if (blockState == 1) {
+				maze[i][j].canWalk = false;
+			}
+			else {
+				maze[i][j].canWalk = true;
+			}
+		}
+	}
+
+	cout << solveMaze(maze, 0, 0, n, m);
+
+}
---
+++ onlineAssignment8/8.2. LeetCode59—螺旋矩阵II/8.2. LeetCode59—螺旋矩阵II.cpp
@@ -0,0 +1,100 @@
+﻿/*2. LeetCode59—螺旋矩阵II
+【问题描述】
+给定一个正整数n，生成一个包含1到n^2所有元素，且元素按顺时针顺序螺旋排列的正方形矩阵。
+要求设计如下函数：
+class Solution{
+public:
+ vector<vector<int>> generateMatrix(int n)
+ {  …   }
+};
+【输入形式】
+输入一个正整数n。
+【输出形式】 
+输出一个正方形矩阵，其中元素按顺时针顺序螺旋排列，元素的值为1~n^2之间的正整数。
+【样例输入】
+     3
+【样例输出】[
+[ 1, 2, 3 ],
+[ 8, 9, 4 ],
+[ 7, 6, 5 ]
+]
+【样例说明】 
+输入一个正整数3，输出元素值为1~9的3×3方阵，其元素按顺时针顺序螺旋排列。测试数据存放在in.txt文件中。
+【评分标准】
+共10个测试用例，每通过一个测试得10分。*/
+#include<iostream>
+#include<fstream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n){
+        vector<vector<int>>result(n, vector<int>(n));
+        int data = 1;
+        int i = n;
+        int j = n;
+        int state = 1;
+        while (!(i == 0 && j == 0)) {
+            if (state == 1) {
+                for (int k = 0 + (n - j) / 2; k < 0 + (n - j) / 2 + j; k++) {
+                    result[0 + (n - i) / 2][k] = data;
+                    data++;
+                }
+                i--;
+                state = 2;
+            }
+            else if (state == 2) {
+                for (int k = 0 + (n - i + 1) / 2; k < 0 + (n - i + 1) / 2 + i; k++) {
+                    result[k][n - 1 - (n - j) / 2] = data;
+                    data++;
+                }
+                j--;
+                state = 3;
+            }
+            else if (state == 3) {
+                for (int k = n - 1 - (n - j + 1) / 2; k > n - 1 - (n - j + 1) / 2 - j; k--) {
+                    result[n - 1 - (n - i - 1) / 2][k] = data;
+                    data++;
+                }
+                i--;
+                state = 4;
+            }
+            else if (state == 4) {
+                for (int k = n - 1 - (n - i) / 2; k > n - 1 - (n - i) / 2 - i; k--) {
+                    result[k][0 + (n - j - 1) / 2] = data;
+                    data++;
+                }
+                j--;
+                state = 1;
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+    Solution mySolution;
+    fstream in;
+    in.open("in.txt");
+    int n;
+    cin >> n;
+    in >> n;
+    vector<vector<int>>matrix = mySolution.generateMatrix(n);
+    cout << '[' << endl;
+    for (int i = 0; i < n; i++) {
+        cout << '[';
+        for (int j = 0; j < n; j++) {
+            cout << ' ' << matrix[i][j];
+            if (j != n - 1) {
+                cout << ',';
+            }
+        }
+        cout << ' ' << ']';
+        if (i != n - 1) {
+            cout << ',';
+        }
+        cout << endl;
+    }
+    cout << ']';
+}
